96f07224f25a39ed49646849224ad2d05196b261
emmanuel <manu.acheres@gmail.com>
2025-10-01 12:54:03 +0200

    hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,23 @@
-Print("salut tout le monde, travaille sur Git et Github")
+my_list=["a","b","c","d","e"]
+print(my_list[0])
+print(my_list[2])
+my_list[1] = "youpi"
+print(my_list[1])
+my_list.append(26)
+print(my_list)
+divers = my_list.copy
+print(divers)
+my_list.append("a")
+print(my_list)
+print(my_list.count("a"))
+my_list.extend([2,"bug","coucou",True])
+print(my_list)
+my_list.pop(5)
+print(my_list)
+word = "HELLO"
+#print(word.lower())
+print(word.upper())
+word = word.lower()
+print(word)
+word = word.capitalize()
+print(word)
